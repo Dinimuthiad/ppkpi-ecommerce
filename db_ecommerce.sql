900ca30a47919fefd5875a464188f84684390895
Dinimuthiad <dinimuthiadewi@gmail.com>
2024-07-17 15:03:18 +0700

    Menambah database

--- db_ecommerce.sql
+++ db_ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Jul 05, 2024 at 09:54 AM
+-- Generation Time: Jul 17, 2024 at 10:00 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -62,8 +62,36 @@ CREATE TABLE `level` (
 --
 
 INSERT INTO `level` (`id`, `nama_level`, `keterangan`, `created_at`, `update_at`) VALUES
-(3, 'member', 'aaaaa', '2024-06-28 04:07:39', '2024-06-28 04:07:39'),
-(4, 'aaaaa', 'ssssss', '2024-06-28 06:30:31', '2024-06-28 06:41:34');
+(3, 'Member', 'member', '2024-06-28 04:07:39', '2024-07-12 07:42:58'),
+(4, 'User', 'user', '2024-06-28 06:30:31', '2024-07-12 07:42:43'),
+(5, 'Petugas', 'petugas ', '2024-07-12 03:49:55', '2024-07-12 07:43:02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `setting`
+--
+
+CREATE TABLE `setting` (
+  `id` int(11) NOT NULL,
+  `email_website` varchar(50) NOT NULL,
+  `no_tlp_website` varchar(15) NOT NULL,
+  `alamat_website` text NOT NULL,
+  `ig` varchar(100) NOT NULL,
+  `twitter` varchar(100) NOT NULL,
+  `fb` varchar(100) NOT NULL,
+  `linkedin` varchar(255) NOT NULL,
+  `logo` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `setting`
+--
+
+INSERT INTO `setting` (`id`, `email_website`, `no_tlp_website`, `alamat_website`, `ig`, `twitter`, `fb`, `linkedin`, `logo`, `created_at`, `updated`) VALUES
+(1, 'dinimuthia@gmail.com', '081209329871', ' Jl.Kapinis no.20', 'dini', 'https://x.com/?lang=id', 'https://www.facebook.com/?locale2=id_ID&_rdr', 'dini', '', '2024-07-17 07:38:06', '2024-07-17 07:38:06');
 
 -- --------------------------------------------------------
 
@@ -109,7 +137,11 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `id_level`, `nama_lengkap`, `email`, `password`, `created_at`, `update_at`) VALUES
-(1, 0, 'Dini ', 'dini@gmail.com', 'da39a3ee5e6b4b0d3255bfef95601890afd80709', '2024-06-27 05:23:01', '2024-06-27 08:12:44');
+(1, 3, 'Dini ', 'dini@gmail.com', '7c222fb2927d828af22f592134e8932480637c0d', '2024-06-27 05:23:01', '2024-07-17 01:43:57'),
+(5, 4, 'Dini Muthia ', 'dinim@gmail.com', '7c222fb2927d828af22f592134e8932480637c0d', '2024-07-12 03:49:33', '2024-07-12 07:28:16'),
+(6, 3, 'Dini Muthia Dewi', 'dinimd@gmail.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2024-07-12 03:57:53', '2024-07-12 06:53:15'),
+(7, 5, 'Dini Muthia D', 'dinimdewi@gmail.com', '7c222fb2927d828af22f592134e8932480637c0d', '2024-07-12 04:15:23', '2024-07-12 04:17:52'),
+(9, 5, 'tes', 'a@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', '2024-07-12 07:58:24', '2024-07-12 07:58:24');
 
 --
 -- Indexes for dumped tables
@@ -128,6 +160,12 @@ ALTER TABLE `level`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `setting`
+--
+ALTER TABLE `setting`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `testimoni`
 --
 ALTER TABLE `testimoni`
@@ -137,7 +175,8 @@ ALTER TABLE `testimoni`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_id_level` (`id_level`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -153,7 +192,13 @@ ALTER TABLE `barang`
 -- AUTO_INCREMENT for table `level`
 --
 ALTER TABLE `level`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `setting`
+--
+ALTER TABLE `setting`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `testimoni`
@@ -165,7 +210,17 @@ ALTER TABLE `testimoni`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `fk_id_level` FOREIGN KEY (`id_level`) REFERENCES `level` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
